016be94053c2641557823cf8bc781311a62cc233
hikamac <harimaru27@gmail.com>
2023-09-01 01:41:54 +0900

    add image

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:oracle/models/developer.dart';
 import 'package:oracle/service/developer_service.dart';
-import 'package:oracle/views/pages/sensorTest.dart';
+import 'package:oracle/views/pages/sensor_test.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
